b864ff48189bfc632b3e3598b2747541447de17d
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-12-05 19:50:38 -0800

    Add package.json and gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  pathPrefix: `/portfolio`,
+  siteMetadata: {
+    title: `Aloysius' Portfolio`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+            },
+          },
+        ],
+      },
+    },
+  ],
+}
